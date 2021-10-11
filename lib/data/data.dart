926ad12b2b5037b368657e0e797c733ff5b6510c
ArinKistia22 <arinkistian22@gmail.com>
2021-10-11 14:51:41 +0700

    praktikum 2 - mobile

---
+++ lib/data/data.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_basic/models/news.dart';
+
+List<News> newsData = [
+  News(
+      image: 'images/img2.jpg',
+      title: 'Barcelona Feb 13, 2021',
+      description: 'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+  News(
+      image: 'images/img2.jpg',
+      title: 'Barcelona Feb 13, 2021',
+      description: 'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+  News(
+      image: 'images/img2.jpg',
+      title: 'Barcelona Feb 13, 2021',
+      description: 'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+  News(
+      image: 'images/img2.jpg',
+      title: 'Barcelona Feb 13, 2021',
+      description: 'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+];
